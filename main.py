05535c08c9112ffadd9fd32a99899ca4740e6bcd
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-10-31 18:07:18 -0300

    criando menu cadastro

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ def menu():
     saudacao()
 
     while True:
-        try:
             # criando a variavel de selecao da opcao premium
             clientePremium = str(input("Deseja ser cliente PREMIUM e ter varios beneficios? [S/N] ")).strip().upper()
             while True:
                 if clientePremium not in 'SN':
                     print('Opção Inválida, digite somente S ou N')
-                    break
+                    clientePremium = str(input("Deseja ser cliente PREMIUM e ter varios beneficios? [S/N] ")).strip().upper()
+                else:
+                    break    
                 # condicao caso cliente premium for sim
             if clientePremium in 'S':
                 # criando um dicionario vazio
@@ -23,16 +24,13 @@ def menu():
                 # inserindo os dados na variavel cliente via input
                 cliente["nome"] = str(input("Digite seu Nome:"))
                 cliente["cpf"] = int(input("Digite seu CPF: "))
-                cliente["telefone"]: int(input("Digite Seu Telefone: "))
-                cliente["datanasc"]: int(input("Data de Nascimento: "))
-                # gravando os dados inseridos no dicionario cliente
+                cliente["telefone"] = int(input("Digite Seu Telefone: "))
+                cliente["datanasc"] = int(input("Data de Nascimento: "))
+                # gravando os dados inseridos no dicionario cliente dentro do arquivo txt
                 cadastrarcliente(cliente.copy())
+                break
             else:
                 #caso a opcao seja não irá direto pro menu
                 break
-
-        except(ValueError):
-            print('Opção Inválida\n')
-            sleep(1)    
 
 menu()            
